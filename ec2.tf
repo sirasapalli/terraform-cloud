02ec846340ea033b51071a5a8e75dc84c44cf3ae
sirasapalli <104012861+sirasapalli@users.noreply.github.com>
2022-10-12 19:32:56 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,10 @@
 provider "aws" {
  region = "us-east-1"
 }
-resource "aws_instance" "myec2" 
-{
-   ami = "ami-05fa00d4c63e32376"
-   instance_type = "t2.micro"
-   tags = {
-    Name = "HelloWorld"
-  }  
+resource "aws_instance" "hello" {
+    ami = "ami-05fa00d4c63e32376"
+    instance_type = "t2.micro"
+    tags = {
+      "Name" = "ec2-sg"
+    } 
 }
